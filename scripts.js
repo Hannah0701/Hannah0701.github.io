9848fb36475d7adc4f7a92f143b7a4b0a9af6ea9
Hannah Phillips <hannah.phillips@simplybusiness.co.uk>
2024-05-01 10:36:40 +0100

    made quite a few tweaks to seperate pages, adding a menu to navigate

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
-function myFunction(x) {
+/* When the user clicks on the menuIcon change the grphic and 
+toggle between hiding and showing the dropdown content */
+function menuIcon(x) {
     x.classList.toggle("change");
+    document.getElementById("myDropdown").classList.toggle("show");
   }
+
+// Close the dropdown menu if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.menu.change')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
